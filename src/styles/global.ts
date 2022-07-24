13741094b7f4dd17d8ec74ec9f63ad5bda37ff1c
gianilch <giancilchechen@gmail.com>
2022-07-24 17:50:42 -0300

    First Upload

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   
+   :root {
+     --background: #F8F2F5;
+     --shape: #FFFFFF;
+     --red: #E52E4D;
+     --blue: #5429cc;
+     --blue-light: #6933ff;
+     --text-title: #363f5f;
+     --text-body: #969CB3;
+   }
+
+   * {
+
+       margin: 0;
+       padding: 0;
+       box-sizing: border-box;
+    }
+
+    @media (max-width: 1080px) {
+        font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+        font-size: 87.5%; 
+    }
+
+    body {
+        background: var(--background) ;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disable] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
