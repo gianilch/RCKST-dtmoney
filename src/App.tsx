dce66a2b2af46edf07aa91aa1e3b69c39f1545b6
gianilch <89846332+gianilch@users.noreply.github.com>
2022-07-26 13:04:39 -0300

    New features

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header/Index";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
-      <Header/>
-      <Dashboard/>
-      <GlobalStyle/>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
     </>
   );
 }
